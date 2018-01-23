ce6b555ea469eaaf57508a60b2e29f05f5b3ed67
nicoloko <dev@nicoloko.com>
2018-01-23 07:10:10 +0100

    Move to deploy on GKE

--- delete_kubenertes.sh
+++ delete_kubenertes.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-minikube delete
+kubectl delete -f minikube/redis.yaml
+kubectl delete -f minikube/rabbitmq.yaml
+kubectl delete -f minikube/keystone.yaml
+kubectl delete -f minikube/restapi.yaml
+kubectl delete -f minikube/reader.yaml
+kubectl delete -f minikube/worker.yaml
 
-rm -Rf ~/.kube 
-rm -Rf ~/.minikube 
+ip_registry="eu.gcr.io/opteama-stelia"
 
-docker-machine rm registry-vm --force
+gcloud container images delete --quiet $ip_registry/redis
+gcloud container images delete --quiet $ip_registry/rabbitmq:3-management
+gcloud container images delete --quiet $ip_registry/docker-openstack-keystone
+gcloud container images delete --quiet $ip_registry/flask-app
+gcloud container images delete --quiet $ip_registry/reader-app
+gcloud container images delete --quiet $ip_registry/worker-app
+
+gcloud container clusters delete kubernetes-lab1 --zone europe-west1-d
--- deploy_kubernetes.sh
+++ deploy_kubernetes.sh
@@ -1,59 +1,46 @@
 #!/bin/bash
 
+docker pull redis
+docker pull rabbitmq:3-management
+docker pull garland/docker-openstack-keystone
 
 cd rest-api && docker build -t flask-app . && cd ../worker && docker build -t worker-app .  && cd ../reader && docker build -t reader-app . && cd ..
 
-eval $(docker-machine env -u)
-docker-machine create \
-      --engine-env 'DOCKER_OPTS="-H unix:///var/run/docker.sock"' \
-      --driver virtualbox \
-      registry-vm
 
-eval $(docker-machine env registry-vm)
-docker run -d -p 5000:5000 --name registry registry:2
-eval $(docker-machine env -u)
+ip_registry="eu.gcr.io/opteama-stelia"
+docker tag redis $ip_registry/redis
+docker tag rabbitmq:3-management $ip_registry/rabbitmq:3-management
+docker tag garland/docker-openstack-keystone $ip_registry/docker-openstack-keystone
+docker tag flask-app $ip_registry/flask-app
+docker tag reader-app $ip_registry/reader-app
+docker tag worker-app $ip_registry/worker-app
 
-ip_registry=$(docker-machine ip registry-vm)
-docker tag redis $ip_registry:5000/redis
-docker tag rabbitmq:3-management $ip_registry:5000/rabbitmq:3-management
-docker tag garland/docker-openstack-keystone $ip_registry:5000/docker-openstack-keystone
-docker tag flask-app $(docker-machine ip registry-vm):5000/flask-app
-docker tag reader-app $(docker-machine ip registry-vm):5000/reader-app
-docker tag worker-app $(docker-machine ip registry-vm):5000/worker-app
+gcloud docker -- push $ip_registry/redis
+gcloud docker -- push $ip_registry/rabbitmq:3-management
+gcloud docker -- push $ip_registry/docker-openstack-keystone
+gcloud docker -- push $ip_registry/flask-app
+gcloud docker -- push $ip_registry/reader-app
+gcloud docker -- push $ip_registry/worker-app
 
-docker push $ip_registry:5000/redis
-docker push $ip_registry:5000/rabbitmq:3-management
-docker push $ip_registry:5000/docker-openstack-keystone
-docker push $(docker-machine ip registry-vm):5000/flask-app
-docker push $(docker-machine ip registry-vm):5000/reader-app
-docker push $(docker-machine ip registry-vm):5000/worker-app
-
-minikube start --insecure-registry=$(docker-machine ip registry-vm):5000
-minikube dashboard
-minikube addons enable heapster
+#gcloud container clusters create kubernetes-lab1   --disk-size 100   --zone europe-west1-d   --enable-cloud-logging   --enable-cloud-monitoring   --machine-type n1-standard-2   --num-nodes 3
+#gcloud container clusters get-credentials kubernetes-lab1 --zone europe-west1-d
 
 kubectl create -f minikube/redis.yaml
 kubectl create -f minikube/rabbitmq.yaml
 kubectl create -f minikube/keystone.yaml
 
-echo "Minikube ip: $(minikube ip)"
-echo "Keystone port: $(kubectl get service keystone-management --output jsonpath='{.spec.ports[?(@.port==35357)].nodePort}')"
-
-# echo "Waiting Keystone to launch on 35357..."
-# while ! nc -z $(minikube ip) $(kubectl get service keystone-management --output jsonpath='{.spec.ports[?(@.port==35357)].nodePort}'); do   
-#   sleep 1 # wait for 1/10 of the second before check again
-# done
-
-# echo "Waiting RabbitMQ to launch on 15672..."
-# while ! nc -z $(minikube ip) $(kubectl get service rabbitmq-management --output jsonpath='{.spec.ports[?(@.port==15672)].nodePort}'); do   
-#   sleep 1 # wait for 1/10 of the second before check again
-# done
 
 echo "create apps..."
 kubectl create -f minikube/restapi.yaml
 kubectl create -f minikube/reader.yaml
 kubectl create -f minikube/worker.yaml
 
+sleep 60
+
+
+KEYSTONE_EXTERNAL_IP=`kubectl describe services keystone | grep 'LoadBalancer Ingress' | awk '{print $3}'`
+
+echo "external ip address : ${KEYSTONE_EXTERNAL_IP}"
 
 echo "create user Joe"
 while ! curl -X POST \
@@ -61,29 +48,15 @@ while ! curl -X POST \
     -H "X-Auth-Token:7a04a385b907caca141f" \
     -H "Content-type: application/json" \
     -d '{"user":{"name":"Joe","email":"joe@example.com.com","enabled":true,"password":"1234"}}' \
-    http://$(minikube ip):$(kubectl get service keystone-management --output jsonpath='{.spec.ports[?(@.port==35357)].nodePort}')/v2.0/users; do
+    http://${KEYSTONE_EXTERNAL_IP}:35357/v2.0/users; do
 
     sleep 1
 done
 
-echo "RabbitMQ: http://$(minikube ip):$(kubectl get service rabbitmq-management --output jsonpath='{.spec.ports[?(@.port==15672)].nodePort}')"
-echo "REST API Swagger: http://$(minikube ip):$(kubectl get service restapi-service --output jsonpath='{.spec.ports[?(@.port==5000)].nodePort}')/apidocs/index.html"
-open http://$(minikube ip):$(kubectl get service rabbitmq-management --output jsonpath='{.spec.ports[?(@.port==15672)].nodePort}')
-open http://$(minikube ip):$(kubectl get service restapi-service --output jsonpath='{.spec.ports[?(@.port==5000)].nodePort}')/apidocs/index.html
+RESTAPI_EXTERNAL_IP=`kubectl describe services restapi | grep 'LoadBalancer Ingress' | awk '{print $3}'`
 
+open http://${RESTAPI_EXTERNAL_IP}:5000/apidocs/index.html
 
 # kubectl scale --replicas=10 deployments/worker-deployment
 
 # curl -X DELETE -H "X-Auth-Token:7a04a385b907caca141f" http://$(minikube ip):$(kubectl get service keystone-management --output jsonpath='{.spec.ports[?(@.port==35357)].nodePort}')/v2.0/users/1ce8553ef4ca4f2fbc73efbca8723a2a
-
-curl -X POST \
-    --connect-timeout 1 --max-time 10 \
-    -H "X-Auth-Token:7a04a385b907caca141f" \
-    -H "Content-type: application/json" \
-    -d '{"user":{"name":"Laurent","email":"joe@example.com.com","enabled":true,"password":"123"}}' \
-    http://$(minikube ip):$(kubectl get service keystone-management --output jsonpath='{.spec.ports[?(@.port==35357)].nodePort}')/v2.0/users
-
-
-while ! minikube addons open heapster; do
-    sleep 1
-done
